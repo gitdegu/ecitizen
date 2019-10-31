586aa511e1487d787581708c7604027c6dcc09cc
gitdegu <deguadnewdilu@yahoo.com>
2019-10-31 21:44:07 +0100

    change to TestingNumberOrNot.java

--- src/TestingNumberOrNot.java
+++ src/TestingNumberOrNot.java
@@ -5,9 +5,21 @@ public class TestingNumberOrNot {
 	
 	public static boolean isNumber(String str) {
 		for(int i=0;i<str.length();i++) {
-			if(!Character.isDigit(str.charAt(i))) {
+			if(!Character.isDigit(str.charAt(i)) && str.charAt(i)!='.') {
 				return false;
 			}
+		}
+		return true;
+	}
+	public static boolean findNumberFromACollection(String []strCollection) {
+		int i=0;
+		while(i<strCollection.length) {
+			for(int j=0;j<strCollection[i].length();j++) {
+				if(!Character.isDigit(strCollection[i].charAt(j)) && strCollection[i].charAt(j)!='.') {
+					return false;
+				}
+			}
+			i++;
 		}
 		return true;
 	}
@@ -17,6 +29,8 @@ public class TestingNumberOrNot {
 		System.out.println("Does the" + input +"="+ TestingNumberOrNot.isNumber(input));
 		input="76";
 		System.out.println("Does the" + input +"="+ TestingNumberOrNot.isNumber(input));
+		String []inputStringSet= {"78.54","9t.65"};
+		System.out.println("The String seques " + inputStringSet +"="+ findNumberFromACollection(inputStringSet));
 	}
 
 }
