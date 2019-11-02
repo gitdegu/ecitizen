a8fe6420d0ffd39c2cdfc058275dddd15b0c856e
gitdegu <deguadnewdilu@yahoo.com>
2019-11-02 21:45:32 +0100

    adding hasmap declaration

--- src/HashMapCollections.java
+++ src/HashMapCollections.java
@@ -1,8 +1,13 @@
-
+import java.util.HashMap;
 public class HashMapCollections {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		HashMap<String,String>hmap=new HashMap<String,String>();
+		hmap.put("Eng","English");
+		hmap.put("Mth","Maths");
+		hmap.put("Psy", "Physics");
+		hmap.put("Bio","Biology");
 
 	}
 
